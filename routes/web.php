2e3a156d0bd459e265ba84d6243e0c4186cb2a93
LauchlynDowns <LAUCHLYN1-01@gmail.com>
2022-11-29 23:34:26 +0000

    got database pulling data

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\content;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -18,7 +18,10 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard', [
+        'contents' => content::all()
+    ]);
+    
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/auth.php';
