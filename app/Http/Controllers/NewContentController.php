6cb51fe83d725bc51964b919bdb99450305d1822
LauchlynDowns <LAUCHLYN1-01@gmail.com>
2022-12-05 23:40:27 +0000

    added post creation

--- app/Http/Controllers/NewContentController.php
+++ app/Http/Controllers/NewContentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\Models\content;
+
 // use Illuminate\Http\Request;
 // use App\Models\User;
 class NewContentController extends Controller
@@ -10,14 +11,14 @@ class NewContentController extends Controller
         return view('newcontent.addcontent');
     }
     public function addnewcontent(){
-
+       
   $attributes = request()->validate([
     'userid' =>'required|max:255',
         'contenttitle' =>'required|max:255',
         'contentdescription' => 'required|max:255',
         'contentbody' => 'required'
        ]);
-        //  dd('success validation succeeded');
+       
      content::create($attributes);
    return redirect('/');
     }
